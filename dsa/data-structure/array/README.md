d1a257e1512f844d884f52bbbb1c8edc985f47b9
linhvuquach <quachvulinhag1997@gmail.com>
2024-06-09 18:47:13 +0700

    [dsa-16] feat: array data structure

---
+++ dsa/data-structure/array/README.md
@@ -0,0 +1,7 @@
+# Array
+
+An array is a collection of items of the **same variable type** stored that are stored at **contiguous** memory locations. It’s one of the most popular and simple data structures and is often used to implement other data structures. Each item in an array is **indexed** starting with 0.
+
+## References
+
+- https://www.geeksforgeeks.org/top-data-structures-that-every-programmer-must-know/
---
+++ dsa/data-structure/array/implementation/dynamic-array/DynamicArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using DynamicArray;
+
+DynamicArray<String> dynamicArray =
+[
+    "L",
+    "i",
+    "n",
+    "h",
+    "V",
+    "u",
+    "Q",
+    "u",
+    "a",
+    "c",
+    "h",
+];
+
+dynamicArray.Add(dynamicArray.Size(), "2024");
+
+Console.WriteLine(dynamicArray.ToString());
+Console.WriteLine($"Array size is: {dynamicArray.Size()}");
+Console.WriteLine($"Item at 3: {dynamicArray.Get(3)}");
+Console.WriteLine($"Index of Q item: {dynamicArray.IndexOf("Q")}");
+
+dynamicArray.RemoveAt(7);
+
+foreach (String? item in dynamicArray)
+{
+    Console.WriteLine(item);
+}
+
+dynamicArray.Clear();
+Console.WriteLine(dynamicArray.ToString());
+Console.WriteLine("Get Element at 3:" + dynamicArray.Get(3));
+Console.WriteLine($"Array size is: {dynamicArray.Size()}");
