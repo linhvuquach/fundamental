d41914931afdc6eaf931ce4b2758cd8480f6f4e3
linh.vu.quach <quachvulinhag1997@gmail.com>
2024-06-16 22:10:35 +0700

    [dsa-16] feat: queue implementation

---
+++ dsa/data-structure/queue/implementation/Basis/QueueBasedLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using DoublyLinkedList;
+
+namespace QueueBasis;
+
+public class QueueBasedLinkedList<T> : IQueue<T>
+{
+    private DoublyLinkedList<T> list = new DoublyLinkedList<T>();
+
+    public T Dequeue()
+    {
+        if (IsEmpty()) throw new InvalidOperationException("Queue is empty");
+
+        T value = list.PeekLast();
+        list.RemoveLast();
+
+        return value;
+    }
+
+    public void Enqueue(T element)
+    {
+        list.AddFirst(element);
+    }
+
+    public bool IsEmpty()
+    {
+        return list.IsEmpty();
+    }
+
+    public T Peek()
+    {
+        return list.PeekLast();
+    }
+
+    public int Size()
+    {
+        return list.Size();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return list.GetEnumerator();
+    }
+
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
