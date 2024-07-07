e604ca273338bf8a85060e4a31c67890d493b1d4
linhvuquach <quachvulinhag1997@gmail.com>
2024-07-07 15:33:26 +0700

    [fd-05] feat: Solid principles

---
+++ principles/solid/example/dip/Dip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Company.ClassLibrary1;
+
+// Violates DIP
+public class EmailNotifier
+{
+    public void SendEmail(string message)
+    {
+        Console.WriteLine($"Email sent: {message}");
+    }
+}
+
+public class NotificationService
+{
+    private EmailNotifier _emailNotifier = new EmailNotifier();
+
+    public void Notify(string message)
+    {
+        _emailNotifier.SendEmail(message);
+    }
+}
+
+// Follow DIP
+public interface INotifier
+{
+    void SendNotification(string message);
+}
+
+public class EmailNotifier : INotifier
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine($"Email sent: {message}");
+    }
+}
+
+public class SmsNotifier : INotifier
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine($"SMS sent: {message}");
+    }
+}
+
+public class NotificationService
+{
+    public INotifier _notifier;
+
+    public NotificationService(INotifier notifier)
+    {
+        _notifier = notifier;
+    }
+
+    public void Notify(string message)
+    {
+        _notifier.SendNotification(message);
+    }
+}
---
+++ principles/solid/example/lsp/Lsp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Company.ClassLibrary1;
+
+// Violates LSP
+public class Rectangle
+{
+    public virtual double Width { get; set; }
+    public virtual double Height { get; set; }
+
+    public virtual int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Rectangle
+{
+    public override double Width
+    {
+        set { base.Width = base.Height = value; }
+    }
+
+    public override double Height
+    {
+        set { base.Height = base.Width = value; }
+    }
+}
+
+// Follow LSP
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Shape
+{
+    public double Side { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Side * Side;
+    }
+}
